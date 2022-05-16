6e3b043688e002f41da731d37e47deaea15cd8cd
497664492@qq.com <497664492@qq.com>
2022-05-16 14:16:56 +0800

    优化代码

--- web/src/main/java/com/roffer/web/websocket/WebSocketServer.java
+++ web/src/main/java/com/roffer/web/websocket/WebSocketServer.java
@@ -2,7 +2,6 @@ package com.roffer.web.websocket;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.roffer.common.http.R;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
